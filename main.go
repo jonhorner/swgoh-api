02edc9bfeca39bac1a302f31f5a347fdf220e423
Jon Horner <jon.horner@gmail.com>
2024-05-14 09:00:30 +0100

    Update Guild - Make update conditional

--- main.go
+++ main.go
@@ -20,10 +20,11 @@ const API_UNIT_URL = API_BASE_URL + "units/"
 // const API_CHARACTERS_URL = self::API_BASE_URL . '/api/characters/';
 
 func main() {
+	updateGuild := false
 	// 1) Get list of required units
 	//
 	// 2) Get list of active users and their rosters - http://api.swgoh.gg/guild-profile/guild_code
-	//
+	//		- store ally code, unit key, relic level, gear level, GP (for getting best person to farm a unit)])
 	// 3) Get count of characters required - go we have that many characters?
 	//
 	err := godotenv.Load()
@@ -31,32 +32,34 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
-	c := guild.Credentials{
-		Username: os.Getenv("USERNAME"),
-		Password: os.Getenv("PASSWORD"),
-		Url: "http://api.swgoh.gg/guild-profile/6Q1Rhhi0T26BnkByV1NmxQ",
-	}
-
-	data := guild.GetMembers(c)
-	content, err := json.Marshal(data.Data.Members)
-	dataStore := db.StoreMemberData{
-		Table: "SwgohGuildData",
-		KeyName: "SwgohGuild",
-		KeyValue: "6Q1Rhhi0T26BnkByV1NmxQ",
-		ValueName: "MemberData",
-		Value: string(content),
-	}
-
-	db.StoreGuildMembers(dataStore)
-
-	getUnits := false
-	if getUnits == true {
-		c := units.Credentials{
+	if updateGuild {
+		c := guild.Credentials{
 			Username: os.Getenv("USERNAME"),
 			Password: os.Getenv("PASSWORD"),
-			Url: API_UNIT_URL,
+			Url: "http://api.swgoh.gg/guild-profile/6Q1Rhhi0T26BnkByV1NmxQ",
 		}
-		data := units.GetUnits(c)
-		log.Println(data.Data[0])
+
+		data := guild.GetMembers(c)
+		content, err := json.Marshal(data.Data.Members)
+		dataStore := db.StoreMemberData{
+			Table: "SwgohGuildData",
+			KeyName: "SwgohGuild",
+			KeyValue: "6Q1Rhhi0T26BnkByV1NmxQ",
+			ValueName: "MemberData",
+			Value: string(content),
+		}
+
+		db.StoreGuildMembers(dataStore)
+
+		getUnits := false
+		if getUnits == true {
+			c := units.Credentials{
+				Username: os.Getenv("USERNAME"),
+				Password: os.Getenv("PASSWORD"),
+				Url: API_UNIT_URL,
+			}
+			data := units.GetUnits(c)
+			log.Println(data.Data[0])
+		}
 	}
 }
